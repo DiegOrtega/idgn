f1d01e987d9a325b1ab3cde364166b8a95653bee
DiegOrtega <nemesisrda@gmail.com>
2017-02-08 00:15:03 -0600

    cambio 5

--- index.js
+++ index.js
@@ -22,6 +22,106 @@ app.get('/activity.ejs', function(request, response) {
   response.render('pages/activity.ejs');
 });
 
+app.get('/ajax_for_index.ejs', function(request, response) {
+  response.render('pages/ajax_for_index');
+});
+
+app.get('/author-edit.ejs', function(request, response) {
+  response.render('pages/author-edit');
+});
+
+app.get('/author-login.ejs', function(request, response) {
+  response.render('pages/author-login');
+});
+
+app.get('/author.ejs', function(request, response) {
+  response.render('pages/author');
+});
+
+app.get('/blog-2.ejs', function(request, response) {
+  response.render('pages/blog-2');
+});
+
+app.get('/blog-3.ejs', function(request, response) {
+  response.render('pages/blog-3');
+});
+
+app.get('/blog-detail-2.ejs', function(request, response) {
+  response.render('pages/blog-detail-2');
+});
+
+app.get('/blog-detail.ejs', function(request, response) {
+  response.render('pages/blog-detail');
+});
+
+app.get('/blog.ejs', function(request, response) {
+  response.render('pages/blog');
+});
+
+app.get('/contact-us.ejs', function(request, response) {
+  response.render('pages/contact-us');
+});
+
+app.get('/faq.ejs', function(request, response) {
+  response.render('pages/faq');
+});
+
+app.get('/gallery.ejs', function(request, response) {
+  response.render('pages/gallery');
+});
+
+app.get('/login.ejs', function(request, response) {
+  response.render('pages/login');
+});
+
+app.get('/messages-2.ejs', function(request, response) {
+  response.render('pages/messages-2');
+});
+
+app.get('/messages.ejs', function(request, response) {
+  response.render('pages/messages');
+});
+
+app.get('/organization.ejs', function(request, response) {
+  response.render('pages/organization');
+});
+
+app.get('/page1.ejs', function(request, response) {
+  response.render('pages/page1');
+});
+
+app.get('/page2.ejs', function(request, response) {
+  response.render('pages/page2');
+});
+
+app.get('/page3.ejs', function(request, response) {
+  response.render('pages/page3');
+});
+
+app.get('/people.ejs', function(request, response) {
+  response.render('pages/people');
+});
+
+app.get('/search.ejs', function(request, response) {
+  response.render('pages/search');
+});
+
+app.get('/shortcodes.ejs', function(request, response) {
+  response.render('pages/shortcodes');
+});
+
+app.get('/site-map.ejs', function(request, response) {
+  response.render('pages/site-map');
+});
+
+app.get('/statictics.ejs', function(request, response) {
+  response.render('pages/statictics');
+});
+
+app.get('/work.ejs', function(request, response) {
+  response.render('pages/work');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
